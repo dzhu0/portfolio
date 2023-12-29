a10a8badf2aaf81b1bbaf9a66fc9136daec24d4a
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-29 14:48:59 -0600

    Create index.jsx
    Export all the default exports as named exports to facilitate the import.

---
+++ components/index.jsx
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header"
+export { default as Main } from "./Main"
+export { default as Footer } from "./Footer"
+export { default as ProjectCard } from "./ProjectCard"
+export { default as ProjectCardPlaceholder } from "./ProjectCardPlaceholder"
