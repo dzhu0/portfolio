4fcbf221f20b86775f4f0b5733871ba2712785f1
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-29 14:42:18 -0600

    Update App.jsx
    Use react-router-dom to handle navigation. App will render the Header, Main, and Footer components wrapped with PortfolioContext. Context will be used in the Header and Main.

--- App.jsx
+++ App.jsx
@@ -2,13 +2,16 @@ import React from "react"
 import { BrowserRouter } from "react-router-dom"
 
 import { Header, Main, Footer } from "./components"
+import { PortfolioContextProvider } from "./PortfolioContext"
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Header />
-            <Main />
-            <Footer />
+            <PortfolioContextProvider>
+                <Header />
+                <Main />
+                <Footer />
+            </PortfolioContextProvider>
         </BrowserRouter>
     )
 }
