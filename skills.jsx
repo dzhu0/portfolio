64590ac32783d52fb3f09ce5df219345274e3861
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-31 19:42:37 -0600

    Create skills.jsx
    Array of skills category with the skill's name and icon.

---
+++ skills.jsx
@@ -0,0 +1,82 @@
+import { faJs, faReact, faBootstrap, faHtml5, faCss3, faPhp, faPython, faNodeJs, faNpm, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faScrewdriverWrench, faDatabase, faListCheck, faServer } from "@fortawesome/free-solid-svg-icons"
+
+export const frontEndSkills = [
+    {
+        name: "JavaScript",
+        icon: faJs,
+    },
+    {
+        name: "React",
+        icon: faReact,
+    },
+    {
+        name: "AJAX",
+        icon: faJs,
+    },
+    {
+        name: "Bootstrap",
+        icon: faBootstrap,
+    },
+    {
+        name: "HTML",
+        icon: faHtml5,
+    },
+    {
+        name: "CSS",
+        icon: faCss3,
+    },
+]
+
+export const backEndSkills = [
+    {
+        name: "PHP",
+        icon: faPhp,
+    },
+    {
+        name: "MySQL",
+        icon: faDatabase,
+    },
+    {
+        name: "Python",
+        icon: faPython,
+    },
+    {
+        name: "PhpMyAdmin",
+        icon: faDatabase,
+    },
+]
+
+export const toolsSkills = [
+    {
+        name: "XAMPP",
+        icon: faScrewdriverWrench,
+    },
+    {
+        name: "Composer",
+        icon: faListCheck,
+    },
+    {
+        name: "NVM",
+        icon: faNodeJs,
+    },
+    {
+        name: "NPM",
+        icon: faNpm,
+    },
+]
+
+export const otherSkills = [
+    {
+        name: "GitHub",
+        icon: faGithub,
+    },
+    {
+        name: "Netlify",
+        icon: faServer,
+    },
+    {
+        name: "InfinityFree",
+        icon: faServer,
+    },
+]
