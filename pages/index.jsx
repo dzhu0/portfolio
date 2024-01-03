34fdbafb15ff6fcc06da3371ed510df98c4c707f
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-01-02 19:30:05 -0600

    Relocated

--- pages/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home"
-export { default as About } from "./About"
-export { default as Experience } from "./Experience"
-export { default as Portfolio } from "./Portfolio"
-export { default as Contact } from "./Contact"
-export { default as NotFound } from "./NotFound"
